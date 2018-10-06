0abc8639f18b1b0b614645254aa0a84ed9c91bb1
Eric Schaefer <omg@eric-schaefer.com>
2018-10-06 15:29:02 +0200

    Naming fix

--- src/index.js
+++ src/index.js
@@ -1,27 +1,27 @@
 module.exports = function({ types: t }) {
-	return {
-		visitor: {
-			// Remove framer from "require" statements
-			StringLiteral(path) {
-				if (path.node.value === 'framer') {
-					var thing = path.findParent(
-						el => el.node.type === 'VariableDeclaration'
-					);
-					if (thing) thing.remove();
-				}
-			},
-			// Remove framer from import statements
-			ImportDeclaration(path) {
-				if (path.node.source.value === 'framer') {
-					path.remove();
-				}
-			},
-			// Remove propertyControls from statics
-			ClassProperty(path) {
-				if (path.node.key.name === 'propertyControls') {
-					path.remove();
-				}
-			}
-		}
-	};
+  return {
+    visitor: {
+      // Remove framer from "require" statements
+      StringLiteral(path) {
+        if (path.node.value === 'framer') {
+          var requireStatement = path.findParent(
+            el => el.node.type === 'VariableDeclaration'
+          );
+          if (requireStatement) requireStatement.remove();
+        }
+      },
+      // Remove framer from import statements
+      ImportDeclaration(path) {
+        if (path.node.source.value === 'framer') {
+          path.remove();
+        }
+      },
+      // Remove propertyControls from statics
+      ClassProperty(path) {
+        if (path.node.key.name === 'propertyControls') {
+          path.remove();
+        }
+      }
+    }
+  };
 };
