06ebc9a7a9fbdb975f226993802eb1282bb9d805
Eric Schaefer <omg@eric-schaefer.com>
2018-10-08 19:49:13 +0200

    Use babel to build the source

--- src/index.js
+++ src/index.js
@@ -1,36 +1,34 @@
-module.exports = function({ types: t }) {
-  return {
-    visitor: {
-      // Remove framer from "require" statements
-      StringLiteral(path) {
-        if (path.node.value === 'framer') {
-          var requireStatement = path.findParent(
-            el => el.node.type === 'VariableDeclaration'
-          );
-          if (requireStatement) requireStatement.remove();
-        }
-      },
-      // Remove framer from import statements
-      ImportDeclaration(path) {
-        if (path.node.source.value === 'framer') {
-          path.remove();
-        }
-      },
-      // Remove propertyControls from statics
-      ClassProperty(path) {
-        if (path.node.key.name === 'propertyControls') {
-          path.remove();
-        }
-      },
-      // Maybe propertyControls was directly assigned
-      Identifier(path) {
-        if (path.node.name === 'propertyControls') {
-          var assignmentEx = path.findParent(
-            el => el.node.type === 'AssignmentExpression'
-          );
-          if (assignmentEx) assignmentEx.remove();
-        }
+export default ({ types: t }) => ({
+  visitor: {
+    // Remove framer from "require" statements
+    StringLiteral(path) {
+      if (path.node.value === 'framer') {
+        const requireStatement = path.findParent(
+          el => el.node.type === 'VariableDeclaration'
+        );
+        if (requireStatement) requireStatement.remove();
+      }
+    },
+    // Remove framer from import statements
+    ImportDeclaration(path) {
+      if (path.node.source.value === 'framer') {
+        path.remove();
+      }
+    },
+    // Remove propertyControls from statics
+    ClassProperty(path) {
+      if (path.node.key.name === 'propertyControls') {
+        path.remove();
+      }
+    },
+    // Maybe propertyControls was directly assigned
+    Identifier(path) {
+      if (path.node.name === 'propertyControls') {
+        const assignmentEx = path.findParent(
+          el => el.node.type === 'AssignmentExpression'
+        );
+        if (assignmentEx) assignmentEx.remove();
       }
     }
-  };
-};
+  }
+});
